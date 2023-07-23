04551be6ae0ef50b43548d1b69a6d72587c71a7c
zoerawson <zoe.rawson14@gmail.com>
2023-07-23 13:36:58 +1000

    modified exercise 1.1

--- moduleone.js
+++ moduleone.js
@@ -1,41 +1,3 @@
-//moduleone- 1.1//
-//moduleone- 1.1//
-
-// (1) KEYWORD (2) NAME = (3) VALUE
-
-//KEYWORD
-// var - original to Javascript
-// let- allows us to reassign later on
-//      - can initalise it without a value
-// const- cannot be reassign, remains constant
-//      - Must give it a value
-
-// console.log('hello world');
-
-// let pizzaTopping = "cheese";
-// console.log(pizzaTopping);
-
-// let courseCode = "IFQ714";
-// console.log(courseCode);
-
-// let name = 'zoe';
-// let lastName = 'rawson';
-// let age = 30;
-
-// const interestRate = 0.3;
-// console.log(interestRate);
-//If we dont need to reassign a variable use const
-
-//perfroming a task
-
-function greet(courseCode) {
-console.log("Hello! The message is:" + ' ' + courseCode);
-}
-
-greet('IFQ714');
-
-
-///////
 
 function addGreeting(message) {
  
@@ -45,6 +7,5 @@ function addGreeting(message) {
 
 let unitCode = 'IFQ721';
 newMessage = addGreeting(unitCode);
- 
 
 console.log(newMessage);
