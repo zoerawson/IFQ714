51664e96963473ab55252f9708e9c15ff8da5a3f
zoerawson <zoe.rawson14@gmail.com>
2023-07-23 13:24:23 +1000

    completed exercise 1.1

--- moduleone.js
+++ moduleone.js
@@ -1,5 +1,50 @@
-//moduleone
+//moduleone- 1.1//
+//moduleone- 1.1//
 
-console.log("hello");
-console.log("attempttwo");
+// (1) KEYWORD (2) NAME = (3) VALUE
 
+//KEYWORD
+// var - original to Javascript
+// let- allows us to reassign later on
+//      - can initalise it without a value
+// const- cannot be reassign, remains constant
+//      - Must give it a value
+
+// console.log('hello world');
+
+// let pizzaTopping = "cheese";
+// console.log(pizzaTopping);
+
+// let courseCode = "IFQ714";
+// console.log(courseCode);
+
+// let name = 'zoe';
+// let lastName = 'rawson';
+// let age = 30;
+
+// const interestRate = 0.3;
+// console.log(interestRate);
+//If we dont need to reassign a variable use const
+
+//perfroming a task
+
+function greet(courseCode) {
+console.log("Hello! The message is:" + ' ' + courseCode);
+}
+
+greet('IFQ714');
+
+
+///////
+
+function addGreeting(message) {
+ 
+    let greeting = "Hello! The message is: ";
+    return greeting + message;
+}
+
+let unitCode = 'IFQ721';
+newMessage = addGreeting(unitCode);
+ 
+
+console.log(newMessage);
